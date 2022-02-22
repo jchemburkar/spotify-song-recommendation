506a78df1c80337f04c26f1d13671aa55d395786
jchemburkar <jchemburkar@Jonathans-MacBook-Pro.local>
2022-02-21 22:21:14 -0500

    adding broad extracts for generation process

--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,6 @@
 """ file for extracting data from api """
 from client import Client
-from typing import List, Dict
+from typing import List, Dict, Optional
 import pandas as pd
 from schema import TrackSchema
 from utils import split_list_n_sized_chunks
@@ -17,7 +17,18 @@ def get_track(track_id: str) -> Dict:
     return data
 
 
-def get_tracks_audio_features(track_ids: List[str]) -> Dict:
+def get_multiple_tracks(track_ids: List[str]) -> Dict:
+    """ extract  multiple tracks from api """
+    chunks = split_list_n_sized_chunks(track_ids, 50)
+    to_return = []
+    for chunk in chunks:
+        song_ids = ",".join(chunk)
+        track_data = CLIENT.make_spotify_request(f"/tracks?ids={song_ids}")
+        to_return.extend(track_data["tracks"])
+    return to_return
+
+
+def get_tracks_audio_features(track_ids: List[str]) -> List[Dict]:
     """ extract a given track from api """
     chunks = split_list_n_sized_chunks(track_ids, 100)
     to_return = []
@@ -52,12 +63,30 @@ def get_artists_similar_artists(artist_id: str) -> Dict:
     return data
 
 
+def get_spotify_recommendation_by_seed(seed_artists: List[str], seed_genres: List[str], seed_tracks: List[str], optional_args: Optional[Dict] = {}) -> Dict:
+    """takes in up to 5 artists and/or up to 5 tracks as seeds, then returns their recommendation
+    example args: seed_artists, seed_genres, seed_tracks, max_acousticness, etc.
+    """
+    if not seed_artists and not seed_genres and not seed_tracks:
+        return None
+    
+    url = f"/recommendations?market=US"
+    for key, data in [("seed_artists", seed_artists), ("seed_genres", seed_genres), ("seed_tracks", seed_tracks)]:
+        if data:
+            url += f"&{key}={','.join(data)}"
+
+    for key, value in optional_args.items():
+        url = f"{url}&{key}={value}"
+
+    data = CLIENT.make_spotify_request(url)
+    return [TrackSchema().dump(track) for track in data.get("tracks")]
+
 
 ###############
 # model funcs #
 ###############
 
-def get_playlist_data(playlist_id: str):
+def get_playlist_data(playlist_id: str) -> pd.DataFrame:
     """ extract relevant audio features and metadata for a playlist """
     playlist = get_playlist(playlist_id)
     tracks = [track["track"] for track in playlist.get("tracks", {}).get("items", [])]
@@ -65,8 +94,5 @@ def get_playlist_data(playlist_id: str):
     track_audio_data = pd.DataFrame(get_tracks_audio_features(track_metadata["id"].unique()))
 
     # merge metadata and audio features, return
-    data = pd.concat([track_metadata, track_audio_data], axis=1).reindex(track_metadata.index)
+    data = pd.merge(track_metadata, track_audio_data, how="inner", on="id").reindex(track_metadata.index)
     return data
-
-
-
--- src/generate_recommendations.py
+++ src/generate_recommendations.py
@@ -1,11 +1,64 @@
 """ overall file for generating recommendations """
 import argparse
-from extract import get_playlist_data, get_artists_top_tracks, get_artists_similar_artists
+import pandas as pd
+from typing import Set
+from extract import (
+    get_playlist_data,
+    get_multiple_tracks,
+    get_artists_similar_artists,
+    get_artists_top_tracks,
+    get_tracks_audio_features
+)
 
 SAMPLE_PLAYLIST = "3capEdrsV9FqJRYlLXrqsF"  # the commute!
 SAMPLE_ALBUM = "1XGGeqLZxjOMdCJhmamIn8"  # FM, Vince Staples
-SAMPLE_TRACK = "5p8jlDLpuWRiuuNDBPC9bY?si=f2fdbcbf077a4b58"  # Hadouken, Chief Keef
+SAMPLE_TRACK = "5p8jlDLpuWRiuuNDBPC9bY"  # Hadouken, Chief Keef
 SAMPLE_ARTIST = "4ytkhMSAnrDP8XzRNlw9FS"  # Vince Gauraldi Trio
+
+###########
+# extract #
+###########
+
+def _get_playlist_similar_artists(playlist_data: pd.DataFrame) -> Set[str]:
+    """ get the set of artist_ids of all artists in or related to a playlist """
+    print("Finding all artists related to given playlist")
+    track_data = get_multiple_tracks(playlist_data["id"])
+    artist_set = set()
+    for track in track_data:
+        for artist in track["artists"]:
+            if artist["id"] not in artist_set:
+                similar_artists = get_artists_similar_artists(artist["id"])
+                for similar_artist in similar_artists["artists"]:
+                    artist_set.add(similar_artist["id"])
+            artist_set.add(artist["id"])
+    
+    print(f"Found {len(artist_set)} artist ids")
+    return artist_set
+
+
+def get_similar_artists_top_tracks(playlist_data: pd.DataFrame) -> pd.DataFrame:
+    """ given playlist data, pull list of all related artists; grab their top tracks, and filter out those present in playlist """
+    similar_artists = _get_playlist_similar_artists(playlist_data)
+    print("Getting top tracks for artists")
+    top_tracks = set()
+    for artist_id in similar_artists:
+        artists_top_tracks = get_artists_top_tracks(artist_id)
+        for track in artists_top_tracks["tracks"]:
+            top_tracks.add(track["id"])
+
+    print(f"Found {len(top_tracks)} tracks")
+    audio_features = get_tracks_audio_features(list(top_tracks))
+    return audio_features
+
+
+######################
+# overall generation #
+######################
+
+def generate_playlist_recommendations(playlist_id: str) -> None:
+    """ given the playlist id, pull in relevant data and generate recommendations """
+    playlist_data = get_playlist_data(playlist_id)
+    get_similar_artists_top_tracks(playlist_data)
 
 
 ########
@@ -22,8 +75,7 @@ def get_args() -> argparse.Namespace:
 def main():
     """ main func """
     args = get_args()
-    print(get_artists_similar_artists(SAMPLE_ARTIST))
-    # playlist_data = get_playlist_data(args.playlist_id)
+    generate_playlist_recommendations(args.playlist_id)
 
 
 if __name__ == "__main__":
